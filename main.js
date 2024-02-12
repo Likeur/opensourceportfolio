5d33eacd5968c684e6f9e3dde6393c8a09f09ddf
Likeur <jacksimba28@gmail.com>
2024-02-13 01:04:00 +0200

    adding font and redesign everysections

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 import './src/css/output.css'
+
+const videos = document.querySelectorAll('video')
+
+videos.forEach(video =>{
+    video.addEventListener('mouseenter', ()=>{
+        video.play()
+    })
+    video.addEventListener('mouseleave', ()=>{
+        video.pause()
+        video.currentTime=0
+    })
+})
